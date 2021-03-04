8b41b7a47e1394ca512ad60f327d1ec517992e04
james <jguest777@gmail.com>
2021-03-04 14:17:03 -0500

    configured product circle C2A

--- next/components/Products.js
+++ next/components/Products.js
@@ -5,13 +5,19 @@ export default function Products() {
     <div className="products">
       <Grid container spacing={3}>
         <Grid item xs={12} sm={4} className="products__item">
-          <img src="https://images.unsplash.com/photo-1511330823821-0f576490e149?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=664&q=80" />
+          <div className="products__item-overlay shades">
+            <h1 className="products__item-text">Shades</h1>
+          </div>
         </Grid>
         <Grid item xs={12} sm={4} className="products__item">
-          <img src="https://images.unsplash.com/photo-1527017769941-89670d542c68?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80" />
+          <div className="products__item-overlay blinds">
+            <h1 className="products__item-text">Blinds</h1>
+          </div>
         </Grid>
         <Grid item xs={12} sm={4} className="products__item">
-          <img src="https://images.unsplash.com/photo-1554215860-863c77aa9c64?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80" />
+          <div className="products__item-overlay shutters">
+            <h1 className="products__item-text">Shutters</h1>
+          </div>
         </Grid>
       </Grid>
     </div>
